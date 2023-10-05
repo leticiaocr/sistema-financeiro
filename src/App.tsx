313fc5a348b35f1cd644bde7bed16156a3ca6872
leticiaocr <leticiabritooliveirac@gmail.com>
2023-10-05 10:51:47 -0300

    feat: primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return (
+    <div>
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>SISTEMA FINANCEIRO</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          {/*ÁREA DE INFORMAÇÕES GERAIS*/}
+
+          {/*ÁREA DE INSERÇÃO DE INFORMAÇÕES*/}
+
+          {/*TABELA DE ITENS*/}
+        </C.Body>
+      </C.Container>
+    </div>
+  )
+}
+
+export default App;
